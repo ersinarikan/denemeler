a4c63fc99f22ef431feb8c719c783212bd9fc77b
ersinarikan <147282199+ersinarikan@users.noreply.github.com>
2023-10-28 01:12:50 +0300

    Merge branch 'master' of https://github.com/ersinarikan/denemeler.git

--- hafta2/src/hafta2/KAreDikdortgen.java
+++ hafta2/src/hafta2/KAreDikdortgen.java
@@ -6,19 +6,19 @@ public class KAreDikdortgen {
 
 	public static void main(String[] args) {
 		Scanner klavye=new Scanner(System.in);
-		System.out.println("Birinci kenarý gir : ");
+		System.out.println("Birinci KenarÄ± Giriniz : ");
 		short k1=klavye.nextShort();
-		System.out.println("Ýkinci kenarý gir : ");
+		System.out.println("Ä°kinci KenarÄ± Giriniz : ");
 		short k2=klavye.nextShort();
 		short alan, cevre;
 		if(k1==k2) { 
 			System.out.println("Sen Bir karesin");
-			System.out.println("Çevresi : "+(4*k1));
-			System.out.println("Alaný : "+(k1*k1));
+			System.out.println("Ã‡evresi :"+(4*k1));
+			System.out.println("AlanÄ± : "+(k1*k1));
 		}else {
-			System.out.println("Sen Bir dikdörtgensin");
-			System.out.println("Çevresi : "+(2*(k1+k2)));
-			System.out.println("Alaný : "+(k1*k2));
+			System.out.println("Sen Bir dikdï¿½rtgensin");
+			System.out.println("ï¿½evresi : "+(2*(k1+k2)));
+			System.out.println("Alanï¿½ : "+(k1*k2));
 		}
 	} 
 
