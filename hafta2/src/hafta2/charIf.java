fdf1b6350fa72a1694a51ef39a21cafa1eea1d83
ersinarikan <147282199+ersinarikan@users.noreply.github.com>
2023-10-28 01:45:44 +0300

    2hata son komit

---
+++ hafta2/src/hafta2/charIf.java
@@ -0,0 +1,20 @@
+package hafta2;
+
+import java.util.Scanner;
+
+public class charIf {
+
+	public static void main(String[] args) {
+	
+		System.out.println("gösterilecek harfi giriniz ");
+		Scanner klavye= new Scanner(System.in);
+		char gosterge= klavye.next().charAt(0);
+		System.out.println(gosterge);
+		
+		
+		
+		
+		
+	}
+
+}
--- hafta2/src/hafta2/kared.java
+++ hafta2/src/hafta2/kared.java
@@ -14,24 +14,28 @@ public class kared {
 		k2 = giris.nextByte();
 		if (k1 == k2) {
 			System.out.println("Bu bir Karedir");
-			System.out.println("Bu karenin Çevresini hesaplamak için (C) Alanını hesaplamak için (A) giriniz : ");
+			System.out.println("Bu Karenin Çevresini hesaplamak için (C) Alanını hesaplamak için (A) giriniz : ");
 			char k = giris.next().charAt(0);
 			int cevre = (2 * (k1 + k2));
-			if (k == 'C' || k=='c' ) {
+			if (k == 'C' || k == 'c') {
 				System.out.println("Bu karenin çevresi = " + cevre);
-			} else if (k == 'A'|| k=='a') {
+				// else if (k !='C' || k !='c'); {System.out.println("yanlış seçim yaptınız");}
+			} else if (k == 'A' || k == 'a') {
 				int alan = (k1 * k2);
+
+				// else if (k !='A' || k !='a'); {System.out.println("yanlış seçim yaptınız");}
 				System.out.println("Bu karenin alanı = " + alan);
+
 			}
 
 		} else {
 			System.out.println("Bu bir dikdörtgendir");
-			System.out.println("Bu Dikdörtgenin Çevresini hesaplamak için (C) Alanını hesaplamak için (A) giriniz ");
+			System.out.println("Bu Dikdörtgenin Çevresini Hesaplamak için (C) Alanını hesaplamak için (A) giriniz ");
 			char d = giris.next().charAt(0);
-			if (d == 'C'|| d=='c') {
+			if (d == 'C' || d == 'c') {
 				int cevred = 2 * (k1 + k2);
 				System.out.println("Bu dikdörgenin Çevresi = " + cevred);
-			} else if (d == 'A' || d=='a') {
+			} else if (d == 'A' || d == 'a') {
 				int aland = k1 * k2;
 				System.out.println("Bu dikdörgenin Alanı = " + aland);
 			}
