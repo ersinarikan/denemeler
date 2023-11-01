27b7e649618c199ae0c46cb5e8381d70328df992
ersinarikan <147282199+ersinarikan@users.noreply.github.com>
2023-11-02 02:45:02 +0300

    hafta33

--- hafta3/src/hafta3/fatura.java
+++ hafta3/src/hafta3/fatura.java
@@ -18,30 +18,41 @@ public class fatura {
 
 			// Değişken tanımları burada bitti.//
 
-			System.out.println("Cep Telefonu Fatura Hsaplama Uygulamasına Hoşgeldiniz");
-			System.out.println("Cep Telefonu Numarasını başında 0 ve boşluk olmadan giriniz");
+			System.out.println("Cep Telefonu Fatura Hesaplama Uygulamasına Hoşgeldiniz");
+			System.out.println("Cep telefonu numarasını başında 0 ve boşluk olmadan giriniz");
 			Scanner giris = new Scanner(System.in);
 			telefonNumarasi = giris.next();
+			
+//			  if (telefonNumarasi=="") 
+//			  { System.out. println("Bir telefon numarası girmediniz. Hesaplamaya devam edebilmek için bir numara girmelisiniz." ); 
+//			  	while(telefonNumarasi=="") 
+//			  	{ 
+//			  		System.out. println("Bir telefon numarası girmediniz. Hesaplamaya devam edebilmek için bir numara girmelisiniz."); 
+//			  		telefonNumarasi = giris.next(); 
+//			  	} 
+//			  }
+			 
 
 			System.out.println(telefonNumarasi + " Numaralı hatta SMS paketi tanımlımı (E/H) ");
 			cevapSms = giris.next().charAt(0);
-			if (cevapSms == 'e' || cevapSms == 'E') {
-				System.out.println(
-						"SMS Kampanyasını Seçiniz\n 1. 100 SMS e kadar Bizden\n 2. 500 SMS e kadar bizden\n 3. 1000 SMS e kadar bizden");
+			if (cevapSms == 'e' || cevapSms == 'E') 
+			{
+				System.out.println("SMS Kampanyasını Seçiniz\n 1. 100 SMS e kadar Bizden\n 2. 500 SMS e kadar bizden\n 3. 1000 SMS e kadar bizden");
 				kampanyaSms = giris.nextByte();
 			}
 
 			System.out.println(telefonNumarasi + " Numaralı hatta Konuşma paketi tanımlımı (E/H) ");
 			cevapKonusma = giris.next().charAt(0);
-			if (cevapKonusma == 'E' || cevapKonusma == 'e') {
-				System.out.println(
-						"Konuşma Kampanyasını Seçiniz\n 1. 1000dk Bizden\n 2. 1500dk Bizden\n 3. 2000dk Bizden");
+			if (cevapKonusma == 'E' || cevapKonusma == 'e') 
+			{
+				System.out.println("Konuşma Kampanyasını Seçiniz\n 1. 1000dk Bizden\n 2. 1500dk Bizden\n 3. 2000dk Bizden");
 				kampanyaKonusma = giris.nextByte();
 			}
 
 			System.out.println(telefonNumarasi + " Numaralı hatta İnternet paketi tanımlımı (E/H) ");
 			cevapInternet = giris.next().charAt(0);
-			if (cevapInternet == 'E' || cevapInternet == 'e') {
+			if (cevapInternet == 'E' || cevapInternet == 'e') 
+			{
 				System.out.println("Internet Kampanyasını Seçiniz\n 1. 1GB Bizden\n 2. 5GB Bizden\n 3. 10GB Bizden");
 				kampanyaInternet = giris.nextByte();
 			}
@@ -56,27 +67,29 @@ public class fatura {
 
 			switch (kampanyaSms) {
 			case 1:
-				if (kullanimSms > 100) {
+				if (kullanimSms > 100) 
+				{
 					faturaSms = (float) (((kullanimSms - 100) * 1.5) + 50);
 				}
 
-				else
-					faturaSms = 50;
+				else faturaSms = 50;
 				break;
+				
 			case 2:
 
-				if (kullanimSms > 500) {
+				if (kullanimSms > 500) 
+				{
 					faturaSms = (float) (((kullanimSms - 500) * 1.5) + 60);
 				}
 
-				else
-					faturaSms = 60;
+				else faturaSms = 60;
 				break;
 			case 3:
-				if (kullanimSms > 1000) {
-					faturaSms = (float) (((kullanimSms - 500) * 1.5) + 70);
-				} else
-					faturaSms = 70;
+				if (kullanimSms > 1000) 
+				{
+					faturaSms = (float) (((kullanimSms - 1000) * 1.5) + 70);
+				} 
+				else faturaSms = 70;
 				break;
 			default:
 				faturaSms = (float) (kullanimSms * 1.5);
@@ -85,27 +98,27 @@ public class fatura {
 
 			switch (kampanyaInternet) {
 			case 1:
-				if (kullanimInternet > 1000) {
+				if (kullanimInternet > 1000) 
+				{
 					faturaInternet = (float) (((kullanimInternet - 1000) * 0.15) + 50);
 				}
 
-				else
-					faturaInternet = 50;
+				else faturaInternet = 50;
 				break;
 			case 2:
 
-				if (kullanimInternet > 5000) {
+				if (kullanimInternet > 5000) 
+				{
 					faturaInternet = (float) (((kullanimInternet - 5000) * 0.15) + 60);
 				}
-
-				else
-					faturaInternet = 60;
+				else faturaInternet = 60;
 				break;
 			case 3:
-				if (kullanimInternet > 10000) {
+				if (kullanimInternet > 10000) 
+				{
 					faturaInternet = (float) (((kullanimInternet - 10000) * 0.15) + 70);
-				} else
-					faturaInternet = 70;
+				} 
+				else faturaInternet = 70;
 				break;
 			default:
 				faturaInternet = (float) (kullanimInternet * 0.15);
@@ -114,40 +127,36 @@ public class fatura {
 
 			switch (kampanyaKonusma) {
 			case 1:
-				if (kullanimKonusma > 1000) {
+				if (kullanimKonusma > 1000) 
+				{
 					faturaKonusma = (float) (((kullanimKonusma - 1000) * 0.15) + 50);
 				}
-
-				else
-					faturaKonusma = 50;
+				else faturaKonusma = 50;
 				break;
 			case 2:
 
-				if (kullanimKonusma > 1500) {
+				if (kullanimKonusma > 1500) 
+				{
 					faturaKonusma = (float) (((kullanimKonusma - 1500) * 0.15) + 60);
 				}
-
-				else
-					faturaKonusma = 60;
+				else faturaKonusma = 60;
 				break;
 			case 3:
-				if (kullanimKonusma > 2000) {
+				if (kullanimKonusma > 2000) 
+				{
 					faturaKonusma = (float) (((kullanimKonusma - 2000) * 0.15) + 70);
-				} else
-					faturaKonusma = 70;
+				} 
+				else faturaKonusma = 70;
 				break;
 			default:
 				faturaKonusma = (float) (kullanimKonusma * 0.15);
 			}
 			System.out.println("Özel Tüketim Vergisini Giriniz ");
-
 			otv = giris.nextInt();
-			fatura = ((faturaKonusma + faturaInternet + faturaSms)
-					+ (((faturaKonusma + faturaInternet + faturaSms) * otv) / 100));
-
-			System.out.println(" Fatura SMS " + faturaSms + "\n Fatura Internet " + faturaInternet
-					+ " \n Fatura Konuşma " + faturaKonusma);
-			System.out.println(telefonNumarasi + " Numaralı Telefona Ait fatura " + fatura + "TL");
+			
+			fatura = ((faturaKonusma + faturaInternet + faturaSms) + (((faturaKonusma + faturaInternet + faturaSms) * otv) / 100));
+			System.out.println(" Fatura SMS " + faturaSms + "\n Fatura Internet " + faturaInternet	+ " \n Fatura Konuşma " + faturaKonusma);
+			System.out.println(telefonNumarasi + " Numaralı Telefona Ait fatura " + fatura + " TL");
 			System.out.println("Yeni Fatura Hesaplamak İstiyormusun (E/H)");
 
 			devam = giris.next().charAt(0);
