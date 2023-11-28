52380efb2490d3f2ce1eb0f4f153385ac3088bd7
T007195 <ersinarikan66@hotmail.com>
2023-11-28 17:41:42 +0300

    commit4

--- hafta7/src/hafta7/FonkKitaplik.java
+++ hafta7/src/hafta7/FonkKitaplik.java
@@ -1,4 +1,5 @@
 package hafta7;
+
 import java.util.Scanner;
 
 public class FonkKitaplik {
@@ -6,29 +7,33 @@ public class FonkKitaplik {
 	public static void kitapEkle(Scanner scanner, String[] kitapAdi, String[] yazarAdi, int[] sayfaSayisi,
 			int[] basimYili, int ks) {
 		scanner.nextLine();
-		System.out.print("Kitap ad� : ");
+		System.out.print("Kitap adı : ");
 		kitapAdi[ks] = scanner.nextLine();
-		System.out.print("Yazar ad� : ");
+		System.out.print("Yazar adı : ");
 		yazarAdi[ks] = scanner.nextLine();
-		System.out.print("Sayfa Say�s�");
+		System.out.print("Sayfa Sayısı");
 		sayfaSayisi[ks] = scanner.nextInt();
-		System.out.print("Bas�m y�l�");
+		System.out.print("Basım yılı");
 		basimYili[ks] = scanner.nextInt();
 	}
 
 	public static void Listele(String kitapAdi, String yazarAdi, int sayfaSayisi, int basimYili) {
-		System.out.println("Kitap ad�: " + kitapAdi);
-		System.out.println("Yazar ad�: " + yazarAdi);
-		System.out.println("Bas�m Y�l� : " + basimYili);
-		System.out.println("Sayfa Say�s� : " + sayfaSayisi);
+		System.out.println("Kitap adı: " + kitapAdi);
+		System.out.println("Yazar adı: " + yazarAdi);
+		System.out.println("Basım Yılı : " + basimYili);
+		System.out.println("Sayfa Sayısı : " + sayfaSayisi);
 		System.out.println("---------------------------------");
 	}
 
 	public static void YazarArama(String[] kitapAdi, String[] yazarAdi, int[] sayfaSayisi, int[] basimYili,
 			String arananYazar) {
 		for (int i = 0; i < basimYili.length; i++) {
-			if (yazarAdi[i].equalsIgnoreCase(arananYazar))
+			// for (int i = 0; i < 3; i++) {
+			if (yazarAdi[i].equalsIgnoreCase(arananYazar)) {
+				// System.out.printf("yazaradı\n " +yazarAdi[i], "aranan yazar\n "+arananYazar);
+				// if (yazarAdi[i]==arananYazar) {
 				Listele(kitapAdi[i], yazarAdi[i], sayfaSayisi[i], basimYili[i]);
+			}
 		}
 
 	}
@@ -41,14 +46,45 @@ public class FonkKitaplik {
 				if (basimYili[i] > enbBasimYili) {
 					enbBasimYili = basimYili[i];
 					yilindex = i;
-					System.out.println("indis "+yilindex);
+					// System.out.println("indis "+yilindex);
 				}
 			}
 			Listele(kitapAdi[yilindex], yazarAdi[yilindex], sayfaSayisi[yilindex], basimYili[yilindex]);
 		} else {
-			System.out.println("Hi� kitap girilmemi�");
+			System.out.println("Hiç kitap girilmemiş");
 		}
 
 	}
 
+	public static void sayfaSayisiOrtalamasi(int[] sayfaSayisi, int ks) {
+		int toplam = 0;
+		for (int i = 0; i < ks; i++) {
+			toplam = toplam + sayfaSayisi[i];
+		}
+		System.out.println("Kütüpanedeki kitapların toplam sayfa sayısı ortalaması: " + toplam / ks);
+	}
+
+	public static void basimYiliORtalamasi(int[] basimYili, int ks) {
+		int toplam = 0;
+		for (int i = 0; i < ks; i++) {
+			toplam = toplam + basimYili[i];
+		}
+		System.out.println("Kütüpanedeki kitapların basım yılı ortalaması: " + toplam / ks);
+
+	}
+
+	public static void ecsKitap(String[] yazarAdi, int[] sayfaSayisi, String arananYazar8) {
+		
+		int ecss = 0;
+		for (int j = 0; j < sayfaSayisi.length; j++) {
+
+			if (yazarAdi[j].equalsIgnoreCase(arananYazar8)) {
+				if (sayfaSayisi[j] > ecss) {
+					ecss = sayfaSayisi[j];
+				}
+			}
+			
+		}
+		System.out.println("yazar " + arananYazar8 + "'ın yazdığı en kalın kitap :" + ecss + " sayfadır");
+	}
 }
--- hafta7/src/hafta7/Kitaplik.java
+++ hafta7/src/hafta7/Kitaplik.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Kitaplik {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Ka� kitap girilecek");
+		System.out.print("Kaç kitap girilecek");
 		int uzunluk = scanner.nextInt();
 		String kitapAdi[] = new String[uzunluk];
 		String yazarAdi[] = new String[uzunluk];
@@ -14,9 +14,9 @@ public class Kitaplik {
 		int ks = 0;
 		int secim = 0;
 		do {
-			System.out.println("1-Kay�t giri�i\n2-Listeleme\n3-Yazar Ad� arama\n4-En Yeni Kitaplar");
-			System.out.println("5-Toplam Kitap Say�s�\n6-Sayfa Say�s� ortalamas�\n7-Bas�m y�l� ortalamas�");
-			System.out.println("8-aranan yazar�n en �ok sayfal� kitab� nedir.\n9-��k��");
+			System.out.println("1-Kayıt girişi\n2-Listeleme\n3-Yazar Adı arama\n4-En Yeni Kitaplar");
+			System.out.println("5-Toplam Kitap Sayısı\n6-Sayfa Sayısı ortalaması\n7-Basım yılı ortalaması");
+			System.out.println("8-Aranan yazarın en çok sayfalı kitabı nedir.\n9-çıkış");
 
 			secim = scanner.nextInt();
 			switch (secim) {
@@ -26,7 +26,7 @@ public class Kitaplik {
 					ks++;
 
 				} else {
-					System.out.println("Dizi dolu, yeni kitap kayd� girilmez.");
+					System.out.println("Dizi dolu, yeni kitap kaydı girilmez.");
 				}
 				break;
 			case 2:
@@ -34,22 +34,35 @@ public class Kitaplik {
 					FonkKitaplik.Listele(kitapAdi[i], yazarAdi[i], sayfaSayisi[i], basimYili[i]);
 				break;
 			case 3:
-				System.out.print("Hangi Yazar�n Kitaplar� : ");
-				String aY = scanner.next();
-				System.out.println("aranan yazar : " + aY);
-				FonkKitaplik.YazarArama(kitapAdi, yazarAdi, sayfaSayisi, basimYili, aY);
+				System.out.print("Hangi Yazarın Kitapları : ");
+				String arananYazar = scanner.next();
+				System.out.println("aranan yazar : " + arananYazar);
+				FonkKitaplik.YazarArama(kitapAdi, yazarAdi, sayfaSayisi, basimYili, arananYazar);
 				break;
 			case 4:
 				FonkKitaplik.EnYeniKitaplar(kitapAdi, yazarAdi, sayfaSayisi, basimYili);
 				break;
 			case 5:
-				System.out.println("K�t�phanedeki toplam kitap say�s� : " + ks);
+				System.out.println("Kütüphanedeki toplam kitap sayısı : " + ks);
 				break;
+			case 6:
+				FonkKitaplik.sayfaSayisiOrtalamasi(sayfaSayisi,ks);
+				break;
+			case 7:
+				FonkKitaplik.basimYiliORtalamasi(basimYili,ks);
+				break;
+			case 8:
+				System.out.println("Aranan yazarı giriniz : ");
+				String arananYazar8= scanner.next();
+				FonkKitaplik.ecsKitap(yazarAdi, sayfaSayisi, arananYazar8 );
+				
+				break;
+				
 			case 9:
 				System.out.println("Program bitti");
 				System.exit(0);
 			default:
-				System.out.println("Yanl�� men� de�eri");
+				System.out.println("Yanlış menü değeri");
 			}
 		} while (secim != 10);
 	}
